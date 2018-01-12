e77e7dd423ab3ccde3f3781c0a78b8d39fad15e3
GCC <gcc@GCCs-MacBook-Pro.local>
2018-01-12 10:00:14 +0100

    added option to return the importance matrix

--- lib/feature_importance.py
+++ lib/feature_importance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-visualize feature importance of a model consists of 
+visualize feature importance of a model consists of
 a feature extractor (pca only currently)
 and a classifier (only linear functions,
                   no tree-based methods currently)
@@ -11,17 +11,13 @@ import pandas as pd
 import numpy as np
 import os
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.decomposition import PCA
-from sklearn.metrics import confusion_matrix
-
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
 
 # draw a overlapping bar graph for pandas dataframes
 def overlapped_bar(df, show=False, width=0.9, alpha=.3,
                    title='', xlabel='', ylabel='', **plot_kwargs):
-    """draw a stacked bar chart except bars on top of 
+    """draw a stacked bar chart except bars on top of
     each other with transparency"""
     pl.figure(figsize=(20, 10))
     xlabel = xlabel or df.index.name
@@ -33,7 +29,7 @@ def overlapped_bar(df, show=False, width=0.9, alpha=.3,
     for i, label, color in zip(range(M), df.columns, colors):
         kwargs = plot_kwargs
         kwargs.update({'color': color, 'label': label})
-        plt.bar(indices, df[label], width=width, 
+        plt.bar(indices, df[label], width=width,
                 alpha=alpha if i else 1, **kwargs)
         plt.xticks(indices + .5 * width,
                    ['{}'.format(idx) for idx in df.index.values])
@@ -44,11 +40,11 @@ def overlapped_bar(df, show=False, width=0.9, alpha=.3,
         plt.show()
     return plt.gcf()
 
-def feature_importance(mean_x,x_cls,pca,classifier,LOG_TRANS=True,
+def display_feature_importance(mean_x,x_cls,pca,classifier,LOG_TRANS=True,
                        TABLE=False,DRAW=False,SAVE_DRAW=False):
     '''
-    visualize feature importance of a model consists of 
-    a feature extractor and a classifier by 
+    visualize feature importance of a model consists of
+    a feature extractor and a classifier by
     drawing/returning the coefficients.
     Input: mean_x, list of mean values of different classes (at least 2)
            x_cls, list of names of the classes
@@ -84,5 +80,3 @@ def feature_importance(mean_x,x_cls,pca,classifier,LOG_TRANS=True,
             plt.savefig('importances.png')
     if TABLE:
         return importance
-    
- 
