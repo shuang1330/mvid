4be7d7f7b289e97ec1a8b358642946604be9a93a
GCC <gcc@GCCs-MacBook-Pro.local>
2018-01-31 17:01:35 +0100

    changed net parameters

--- lib/net.py
+++ lib/net.py
@@ -7,17 +7,21 @@ from tensorflow.contrib.slim import arg_scope
 def autoencoder(x):
     with arg_scope([slim.fully_connected],
     weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
-    weights_regularizer=slim.l2_regularizer(0.0005)):
-        net = slim.fully_connected(x,2)
-        net = slim.fully_connected(net,180)
-    return net
+    weights_regularizer=slim.l2_regularizer(0.5)):
+        net1 = slim.fully_connected(x,50)
+        # net1 = slim.fully_connected(net1,50)
+        # net2 = slim.fully_connected(net1,100)
+        net = slim.fully_connected(net1,214)
+    return net, net1
 
 
 def feedforward_net(x):
     with arg_scope([slim.fully_connected],
     weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
     weights_regularizer=slim.l2_regularizer(0.005)):
-        net = slim.fully_connected(x,90)
+        net = slim.fully_connected(x,100)
         net = slim.fully_connected(net,10)
+        # net = slim.fully_connected(net,10)
         net = slim.fully_connected(net,2)
-    return net
+        cls_prob = tf.nn.softmax(net, name="cls_prob")
+    return net, cls_prob
