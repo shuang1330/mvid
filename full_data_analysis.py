c3db3ef98dffb26d922ea1219fd61e05125bd20d
GCC <gcc@GCCs-MacBook-Pro.local>
2018-01-31 17:06:26 +0100

    fixed some bugs?

--- full_data_analysis.py
+++ full_data_analysis.py
@@ -1,3 +1,7 @@
+# TODO: use compute_class_weight from sklearn
+# now the bender runs the code using the default class_weight
+from math import ceil
+from sklearn.utils.class_weight import compute_class_weight
 import pandas as pd
 import os
 import numpy as np
@@ -32,6 +36,24 @@ from sklearn.model_selection import GridSearchCV
 # validation
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
+
+import tensorflow.contrib.layers as lays
+from sklearn import preprocessing
+import tensorflow as tf
+from lib.net import autoencoder,feedforward_net
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+def dense_to_one_hot(labels_dense, num_classes):
+  """
+  Convert class labels from scalars to one-hot vectors.
+  """
+  # num_labels = labels_dense.shape[0]
+  # index_offset = np.arange(num_labels) * num_classes
+  # labels_one_hot = np.zeros((num_labels, num_classes))
+  # labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+  return np.eye(num_classes)[np.array(labels_dense).reshape(-1)]
+
 
 def load_cadd_annotation(file_path):
     '''
@@ -137,32 +159,118 @@ def read_gavin(gavin_res, labels):
     return sensitivity_g, specificity_g
 
 if __name__ == '__main__':
-    file_path = os.path.join('data','all_variants','cadd_with_info_no_nan_all_imputed.tsv')
+    file_path = os.path.join('data','all_variants',
+                             'cadd_with_info_no_nan_all_imputed.tsv')
     all_variants = load_cadd_annotation(file_path)
+    # print(all_variants.columns)
+    # print(all_variants.head())
+    # raise NotImplementedError
+
     all_variants['INFO'] = all_variants['INFO'].astype('category')
     all_variants['INFO'] = all_variants['INFO'].cat.codes
     for col in all_variants.select_dtypes(exclude=[np.object]).columns:
         null = all_variants[col].isnull().values.sum()
         if null>0:
             all_variants = all_variants.drop([col],axis=1)
-    dummy_all_var=all_variants.drop(['Anc','INFO','chr_pos'],axis=1)
+    dummy_all_var = all_variants.drop(['Anc','INFO','chr_pos','key','POS'],axis=1)
     dummy_all_var = pd.get_dummies(dummy_all_var,sparse=True)
     dummy_all_var['INFO'] = all_variants['INFO']
+    # print(dummy_all_var.columns)
 
     # read data
-    full_data = read_data_set(dummy_all_var,BENCHMARK=False)
-    print('Dataset loaded.',full_data.train.values.shape)
+    # full_data = read_data(dummy_all_var,BENCHMARK=False)
+    mvid = read_data_set(dummy_all_var,BENCHMARK=False)
+    print('Dataset loaded.',mvid.train.values.shape)
+
+    # constant
+    batch_size = 1000
+    epoch_num = 11
+    lr = 0.001
+    batch_per_ep = ceil(mvid.train.num_examples/batch_size)
+
+
+    # ======================= feedfoward networks ==============================
+    # model
+    inputs = tf.placeholder(tf.float32,(None, mvid.train.num_features))
+    labels = tf.placeholder(tf.float32,(None, 2))
+    # ae_outputs = autoencoder(inputs)
+    fn_outputs,fn_probs = feedforward_net(inputs)
+    # loss and training options
+    # loss_ae = tf.reduce_mean((tf.square(ae_outputs-inputs))) # autoencoder
+    # loss_fn = tf.reduce_mean((tf.square(fn_outputs-labels)))
+    loss_fn = tf.reduce_mean(
+                    tf.nn.weighted_cross_entropy_with_logits(
+                    fn_outputs,labels,4))
+    train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss_fn)
+
+    # initializer
+    init = tf.global_variables_initializer()
+
+    # start training
+    with tf.Session() as sess:
+        sess.run(init)
+        print('Start training...')
+        for ep in range(epoch_num):
+            for batch_no in range(batch_per_ep):
+                batch_data, batch_label = mvid.train.next_batch(batch_size)
+                batch_label_onehot = dense_to_one_hot(batch_label,2)
+                _, recon_data,error,probs = sess.run([train_op,
+                                                fn_outputs,
+                                                loss_fn,
+                                                fn_probs],
+                                                feed_dict={inputs:batch_data,
+                                                labels:batch_label_onehot})
+                print('Epoch: {0}\tIteration:{1}\tError: {2}\t'.format(
+                ep, batch_no, error
+                ))
+
+        # test the trained network
+        batch_data, batch_label = mvid.test.values, mvid.test.labels
+        # print(batch_data.shape, batch_label.shape)
+        batch_label_onehot = dense_to_one_hot(batch_label,2)
+        recon_data,error, probs = sess.run([fn_outputs,loss_fn,fn_probs],
+                              feed_dict={inputs:batch_data,
+                              labels:batch_label_onehot})
+
+        # print(batch_label_onehot[:,0])
+        probs = probs[:,0]
+        probs[probs>0.5] = 1
+        probs[probs<0.5] = 0
+        # print(probs)
+        tn, fp, fn, tp = confusion_matrix(batch_label_onehot[:,0],
+                                          probs).ravel()
+        print('Test dataset\tError: {0}'.format(error))
+        print(tn, fp, fn, tp)
+
+
+
+    # # pca LogisticRegression
+    # pipeline_pca = Pipeline(
+    # steps=[('pca',PCA()),
+    # ('logr',LogisticRegression())]
+    # )
+    # param_grid_pca = [
+    # {
+    # 'pca__n_components':np.arange(50,100,50),
+    # 'logr__class_weight':['balanced']
+    # }
+    # ]
+    # classifier_sgd = display_res_gavin_and_best_model(param_grid_pca,
+    #                                  pipeline_pca,
+    #                                  full_data)#,
+    #                                  #filename)
+    # inference(classifier_sgd,myo5b.test)
 
     # ==========================================================
     # Linear model + SGDregressor
     # Parameters
-    n_components = np.arange(10,100,10)
-    loss = ['squared_loss']#, 'huber',
-#            'epsilon_insensitive',
-#            'squared_epsilon_insensitive']
-    penalty = ['elasticnet']
-    l1_ratio = [0.2]
-    learning_rate = ['optimal','invscaling']
+#     n_components = np.arange(10,100,10)
+#     loss = ['squared_loss']#, 'huber',
+# #            'epsilon_insensitive',
+# #            'squared_epsilon_insensitive']
+#     penalty = ['elasticnet']
+#     l1_ratio = [0.2]
+#     learning_rate = ['optimal','invscaling']
     # pipeline
     # param_grid_sgd = [{#'pca__n_components':n_components,
     #                'sgd__penalty':penalty,
@@ -178,34 +286,46 @@ if __name__ == '__main__':
     # pipeline_sgd = Pipeline(steps=[#('pca',PCA()),
     #                            ('sgd',SGDClassifier())])
 
-    # incremental training
-    all_classes = np.array([0,1])
-    classifier_sgd = SGDClassifier(loss='squared_epsilon_insensitive',
-                                   penalty='elasticnet',
-                                   l1_ratio=0.2,
-                                   learning_rate='invscaling',
-                                   warm_start=True,
-                                   eta0=0.1,
-                                   tol=1e-06,
-                                   class_weight='balanced')
-    epoch_num = 10000
-    batch_size=100
-    batch_per_ep = ceil(full_data.train.num_examples/epoch_num)
-    for ep in range(epoch_num):
-        for batch_no in range(batch_per_ep):
-            batch_values,batch_labels = \
-            full_data.train.next_batch(batch_size)
-            classifier_sgd.partial_fit(batch_values,
-                                       batch_label,
-                                       classes=all_classes)
-            print(classifier_sgd.score(full_data.test.values,
-                                       full_data.test.labels))
-
-    # save model
-    filename = os.path.join('model','pca_logr_new.sav')
-    # display results
-    classifier_sgd = display_res_gavin_and_best_model(param_grid_sgd,
-                                     pipeline_sgd,
-                                     full_data)#,
-                                     #filename)
-    inference(classifier_sgd,full_data)
+    # # incremental training
+    # all_classes = np.array([0,1])
+    # class_weight = compute_class_weight('balanced',
+    #                                     all_classes,
+    #                                     full_data.test.labels)
+    # weight_dic = {all_classes[0]:class_weight[0],
+    #               all_classes[1]:class_weight[1]*2}
+    # classifier_sgd = SGDClassifier(loss='squared_epsilon_insensitive',
+    #                                penalty='elasticnet',
+    #                                l1_ratio=0.2,
+    #                                learning_rate='optimal',
+    #                                warm_start=True,
+    #                                eta0=0.01,
+    #                                tol=1e-06,
+    #                                class_weight=weight_dic)
+    # epoch_num = 11
+    # batch_size=10000
+    # batch_per_ep = ceil(full_data.train.num_examples/batch_size)
+    # print(batch_per_ep)
+    #
+    # full_data.train._epochs_completed = 0
+    # for ep in range(epoch_num):
+    #     full_data.train._epochs_completed += 1
+    #     for batch_no in range(batch_per_ep):
+    #         full_data.train._index_in_epoch = batch_no
+    #         print('Epoch: {0}\tIteration:{1}\t'.format(ep,batch_no))
+    #         batch_values,batch_labels = \
+    #         full_data.train.next_batch(batch_size)
+    #         classifier_sgd.partial_fit(batch_values,
+    #                                    batch_labels,
+    #                                    classes=all_classes)
+    #         pred = classifier_sgd.predict(full_data.test.values)
+    #         tn, fp, fn, tp = confusion_matrix(full_data.test.labels,pred).ravel()
+    #         print('TN: {0} FP: {1} Fn: {2} TP: {3}'.format(tn, fp, fn, tp))
+    #         sensitivity,specificity,score = run_display_output(classifier_sgd,full_data.test)
+    #         print('>>> {:.{prec}}\tspecificity: {:.{prec}f}\tauc:{}'.\
+    #         format(sensitivity,specificity,score,prec=3))
+    #
+    #
+    #
+    # # save model
+    # filename = os.path.join('model','incre_linear_fulldata.sav')
+    # # display results
